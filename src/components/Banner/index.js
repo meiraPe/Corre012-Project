9f363abf9f8768c1a599a56cecf0271bfdb9040a
meiraPe <p.meira@aluno.ifsp.edu.br>
2025-08-14 17:24:01 -0300

    Finalizando Banner

---
+++ src/components/Banner/index.js
@@ -0,0 +1,9 @@
+import styles from './Banner.module.css'
+
+export default function Footer() {
+  return (
+    <section className={styles.banner_section}>
+      <a href=''><img src='./banner.jpg'/></a>
+    </section>
+  )
+}
