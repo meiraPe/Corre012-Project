a73f12e4b773aed259e623726082b610a7356263
meiraPe <p.meira@aluno.ifsp.edu.br>
2025-08-14 17:23:31 -0300

    Finalizando Header

---
+++ src/components/Products/index.js
@@ -0,0 +1,13 @@
+import styles from './Products.module.css';
+
+export default function Products() {
+  return (
+    <div className={styles.products}>
+      <section id='produto' className={styles.productSection}>
+        <div className={styles.titleProduct}>
+
+        </div>
+      </section>
+    </div>
+  );
+}
